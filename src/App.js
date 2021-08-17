add117f27f8c7b2389c60450268276f9ff1549c3
WilberCruz <WilberCruz@gmail.com>
2021-08-16 22:16:51 -0600

    Taller2_CC181562

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+
+
+function App() {
+  const [dolar, setDolar] = useState(0);
+  const [euro, setEuro] = useState(0);
+  const [bitcoin, setBitcoin] = useState(0);
+  
+
+  const [tipo, setTipo] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setDolar((total * tipo) * 1);
+    setEuro((total * tipo) * 0.85);
+    setBitcoin((total * tipo) * 0.000022);
+  }, [total, tipo]);
+
+
+  const handleTotalChange = e => {
+    if (!isNaN(e.target.value)) {
+      setTotal(e.target.value);
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <h1>Convertidor de divisas</h1>
+
+      <p>Dolares: {dolar}</p>
+      <p>Euros: {euro}</p>
+      <p>Bitcoin: {bitcoin}</p>
+      <hr />
+
+      <h3>{total}</h3>
+
+      <select onChange={event => setTipo(event.target.value)} value={tipo}>
+        <option value={1}>Dolares</option>
+        <option value={1.17}>Euros</option>
+        <option value={45833.90}>Bitcoin</option>
+      </select>
+      <input onChange={handleTotalChange} value={total} />
+    </div>
+  );
+}
+
+export default App;
